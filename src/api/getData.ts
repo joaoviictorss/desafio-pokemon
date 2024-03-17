c6ab9460b5f51eb73c8517baee02ca9d486fc4c5
João Victor dos Santos Silva <137708191+joaoviictorss@users.noreply.github.com>
2024-03-17 19:51:32 -0300

    call to API of cities is working

---
+++ src/api/getData.ts
@@ -0,0 +1,11 @@
+export async function getData(cityName: string) {
+  const res = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=8f04e1b3301b2ca25146fee52f9efbb7`
+  );
+
+  if (!res.ok) {
+    throw new Error("Cidade não encontrada");
+  }
+
+  return res.json();
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,27 @@
+"use client";
+
+import { getData } from "@/api/getData";
+import { useEffect, useState } from "react";
+
 export default function Home() {
+  const [city, setCity] = useState("");
+  const [cityInfos, setCityInfos] = useState("");
+
+  async function fetchData(local: string) {
+    const data = await getData(local);
+    setCityInfos(JSON.stringify(data));
+  }
+
   return (
-    <h1>Hello world!</h1>
+    <>
+      <input
+        type="text"
+        onChange={(e) => setCity(e.target.value)}
+        value={city}
+      />
+      <button onClick={() => fetchData(city)}>Buscar</button>
+
+      <h1>{cityInfos}</h1>
+    </>
   );
 }
